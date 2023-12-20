819e45f41395cfaf01b9cd5060e3f87d4dc776b1
Anton Termolaev <a.termolaev@mail.ru>
2023-12-20 15:55:21 +0300

    Добавит сложение

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
-import { makeSuccessResponse } from 'utils';
+import { makeSuccessResponse, getSumTwoNums } from '../utils';
+import _ from 'lodash';
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const queryObj = req.query;
+
+  const num1 = queryObj?.num1;
+  const num2 = queryObj?.num2;
+
+  const num1Current = _.isString(num1) ? num1 : '0';
+  const num2Current = _.isString(num2) ? num2 : '0';
+
   const response = {
     auth: false,
+    num1: num1Current,
+    num2: num2Current,
+    sum: getSumTwoNums(num1Current, num2Current),
   }
   res.send(JSON.stringify(response));
 });
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,3 +2,7 @@
 export const makeSuccessResponse = (incomeObj: Record<string, string>) => {
     return JSON.stringify(incomeObj);
 }
+
+export const getSumTwoNums = (num1: string, num2: string) => {
+    return parseFloat(num1) + parseFloat(num2);
+}
